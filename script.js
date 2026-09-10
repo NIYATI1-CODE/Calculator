c7f2895c1c3d3b122645cf30143d2752a0440526
NIYATI1-CODE <niyatisingh629@gmail.com>
2026-09-10 09:15:35 +0000

    feat: add keyboard support

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function divide(a, b) {
   return a / b;
 }
 function operate() {
-  if (num2 !== undefined) {
+  if (num2 !== undefined && num1 !== undefined && operator !== undefined) {
     num1 = Number(num1);
     num2 = Number(num2);
     if (operator == '+') {
@@ -55,7 +55,6 @@ function operate() {
   }
 }
 
-
 number_buttons.forEach(button => {
   button.addEventListener('click', () => {
     number_updater(button.textContent);
@@ -63,113 +62,142 @@ number_buttons.forEach(button => {
   )
 })
 
+
 function number_updater(new_num) {
   if (!operator) {
     if (num1 === undefined || num1 == result) {
-      num1 = `${new_num}`;
+      num1 = new_num;
     }
     else {
       num1 = `${num1}${new_num}`;
     }
-    display.textContent = num1;;
+    display.value = num1;;
   }
   else {
     if (num2 === undefined) {
-      num2 = `${new_num}`;
+      num2 = new_num;
     }
     else {
       num2 = `${num2}${new_num}`;
     }
 
-    display.textContent = `${num1} ${operator} ${num2}`;
+    display.value = `${num1} ${operator} ${num2}`;
 
   }
 
 }
 
 operator_buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    operate();
-    operator = button.textContent;
-    display.textContent = `${num1} ${operator}`;
-  })
+  button.addEventListener('click', () =>
+    operatorFunction(button.textContent))
 })
 
-equal.addEventListener('click', () => {
+
+function operatorFunction(operatorInput) {
+  operate();
+  operator = operatorInput;
+  display.value = `${num1} ${operator}`;
+}
+
+equal.addEventListener('click', equalToOperation);
+
+function equalToOperation() {
   let displayedResult;
   if (num2 !== undefined) {
     displayedResult = operate();
-    display.textContent = `Ans : ${displayedResult}`;
+    display.value = `Ans : ${displayedResult}`;
 
   }
   else if (num1 === undefined) {
-    display.textContent = `ERROR`;
+    display.value = `ERROR`;
   }
   else if (operator !== undefined) {
-    display.textContent = `ERROR`;
+    display.value = `ERROR`;
   }
   else {
-    result = num1;
-    display.textContent = `${displayedResult} = ${displayedResult}`;
+    displayedResult = num1;
+    display.value = `${displayedResult} = ${displayedResult}`;
   }
 
-})
+}
+
+
 
 clear.addEventListener('click', () => {
-  display.textContent = null;
+  display.value = null;
   num1 = undefined;
   num2 = undefined;
   operator = undefined;
   result = undefined;
 }
 )
+decimal.addEventListener('click', decimalAdder);
 
-decimal.addEventListener('click', () => {
+function decimalAdder() {
   if (!operator) {
-    if (!num1.includes('.')) {
-      if (num1 === undefined) {
-        num1 = `0.`;
-        display.textContent = num1;
-      }
-      else {
-        num1 = `${num1}.`;
-        display.textContent = num1;
-      }
+    if (num1 === undefined) {
+      num1 = `0.`;
+      display.value = num1;
+    }
+    else if (!num1.includes('.')) {
+      num1 = `${num1}.`;
+      display.value = num1;
     }
   }
   else {
-    if (!num2.includes('.')) {
-      if (num2 === undefined) {
-        num2 = `0.`;
-        display.textContent = num2;
-      }
-      else {
-        num2 = `${num2}.`;
-        display.textContent = `${num1} ${operator} ${num2}`;
-      }
+    if (num2 === undefined) {
+      num2 = `0.`;
+      display.value = `${num1} ${operator} ${num2}`;
+    }
+    else if (!num2.includes('.')) {
+      num2 = `${num2}.`;
+      display.value = `${num1} ${operator} ${num2}`;
     }
   }
 }
-)
 
-backspace.addEventListener('click', () => {
-  let displayedResult = display.textContent;
+backspace.addEventListener('click', backspaceOperation)
+
+function backspaceOperation() {
+  let displayedResult = display.value;
   if (!operator) {
     num1 = num1.slice(0, -1);
-    display.textContent = num1;
+    display.value = num1;
   }
   else if (displayedResult.indexOf(`${operator}`) === (displayedResult.length - 1)) {
     operator = undefined;
-    display.textContent = num1;
+    display.value = num1;
   }
   else if (displayedResult.lastIndexOf(" ") === (displayedResult.length - 1)) {
-    display.textContent = displayedResult.slice(0, -1);
+    display.value = displayedResult.slice(0, -1);
   }
   else if (num2 !== undefined) {
     num2 = num2.slice(0, -1);
-    display.textContent = `${num1} ${operator} ${num2}`;
+    display.value = `${num1} ${operator} ${num2}`;
     if (num2 == " ") {
       num2 = undefined;
     }
   }
+}
+
+
+display.addEventListener('keydown', (event) => {
+  event.preventDefault();
+  if ('0123456789'.includes(event.key)) {
+    number_updater(event.key)
+  }
+  else if ('+-/*'.includes(event.key)) {
+    operatorFunction(event.key);
+  }
+  else if (event.key === '.') {
+    decimalAdder();
+  }
+  else if (event.key === 'Backspace') {
+    backspaceOperation();
+  }
+  else if (event.key === 'Enter') {
+    equalToOperation();
+  }
+  console.log(event.key, event.repeat)
+
 })
